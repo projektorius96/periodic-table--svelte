cf44d6dbd7af6c4d5fe5f80e08c863cc8aee902c
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-07-08 22:51:11 +0100

    css importing solved, cleaned code, etc.

--- src/modules/Matrix.svelte
+++ src/modules/Matrix.svelte
@@ -7,21 +7,20 @@ import {default as rows, /* hereinafter (non-defaults): */ptable} from "./entrie
 // window.rows = rows
 // window.template = ptable.template
 
-// GOAL : re-write the following 2D block in Svelte way using each block , for more refer to : @https://stackoverflow.com/questions/56520104/svelte-each-function-in-nested-json
-
 let prevRow = 0; // prevent zero due to zero-based array
 </script>
 
 {#each rows as row}
-<tr>
+<span class="element-row">
   <!-- {console.log(row)} -->
   {#each Object.values(row) as col}
-  <td>
+  <span class="element-col">
     <!-- {console.log(col)} -->
-    {col.replace('element', `${prevRow += 1}`)}
-  </td>
+    No.{col.replace('element', `${prevRow += 1}`)}
+    <!-- inner Svelte component with its styling -->
+  </span>
   {/each}
-</tr>
+</span>
 {/each}
 
 <style>
