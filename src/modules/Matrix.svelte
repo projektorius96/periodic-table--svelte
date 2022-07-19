3c095010678aecfb9d3a60afb5f3361ae670b152
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-07-19 23:31:02 +0100

    closer to the point, but still

--- src/modules/Matrix.svelte
+++ src/modules/Matrix.svelte
@@ -1,5 +1,5 @@
 <script>
-import {onMount} from 'svelte'
+import {onMount, afterUpdate} from 'svelte'
 import Element from './Element.svelte'
 import {getInterval} from './array_prototype'
 import {default as rows, /* hereinafter (non-defaults): */ptable} from "./entries.js"; // console.log(rows); # [PASSED]
@@ -14,31 +14,74 @@ let rowID = 0;
 let colID = 0;
 
 let {skipRange: firstRow} = new getInterval(ptable.skipRanges[0].from, 1, ptable.skipRanges[0].to, true, skippedValues([]))
-let toSecondRedir = [];
 let {skipRange: secondRow} = new getInterval(ptable.skipRanges[1].from, 1, ptable.skipRanges[1].to, true, skippedValues([]))
 let {skipRange: thirdRow} = new getInterval(ptable.skipRanges[2].from, 1, ptable.skipRanges[2].to, true, skippedValues([]))
 let totalRange = [...firstRow, ...secondRow, ...thirdRow]; console.log(totalRange);
 // ...
 let allHTMLElements = [];
+let allreHTMLElements = [];
 
 // HELPER :
-window.toSecondRedir = toSecondRedir
 window.allHTMLElements = allHTMLElements
+
+afterUpdate(()=>{
+  console.log("afterUpdate", allHTMLElements)
+  /*   
+  (Array.from(document.getElementsByClassName('element-col'))).forEach((val, index)=>{
+    if (val.style.visibility === "hidden") console.log(val)
+  }) 
+  */
+  firstRow.forEach((x, z)=>{
+    allHTMLElements.splice(x, 1)
+    document.getElementsByName('element').forEach((val, idx)=>{
+
+      if ((x == totalRange[0]) && (val.computedStyleMap().get('visibility').value === "hidden")) {
+        console.log("HIDDEN", val);
+      } 
+      else {
+        if ((x == totalRange[0]) && (val.computedStyleMap().get('visibility').value === "visible")) {
+        console.log("VISIBLE", val);
+        }
+      }
+
+    })
+  })
+  // document.getElementsByName("element").forEach((val, idx)=>{
+  //   // allHTMLElements.push(val)
+  //   console.log("idx, allHTMLElements[idx] (afterUpdate)", idx, allHTMLElements[idx])
+  //   // console.log("parent: ", val.parentElement/* .id */, "at (index): ", idx)
+  //   // console.log(val.computedStyleMap().get('display').value)
+  //   // console.log(val.computedStyleMap().get('display').value === "block");
+  //   /* --- */
+  //   // if (val.computedStyleMap().get('display').value === "block") {
+  //   //   console.log("match", val.parentElement.firstElementChild.textContent, ++idx)
+  //   //   val.parentElement.firstElementChild.textContent = idx
+  //   // }
+  //   // else {
+  //   //   val.parentElement.firstElementChild.textContent = 0;
+  //   // }
+  // })
+})
 
 // Once Original Matrix loaded (mounted) by Svelte, do the modifications : 
 onMount(()=>{
+  console.log("onMount")
   totalRange.forEach((x, z)=>{
-    // console.log("at:", x)
+    console.log("at:", x)
     document.getElementsByName('element').forEach((val, idx)=>{
-      console.log("idx, val", idx, val)
+      if (x == totalRange[0]) allHTMLElements.push(val) // just one cycle not xTimes of totalRange.length
       val.id = ++idx
+      // console.log("inside: ", allHTMLElements[idx])
       // console.log(val.id, typeof val.id /* == string */);
-      // /* --- */
-      // console.log("exploiting idx (totalRange[idx]): ", totalRange[--idx]);
       if (x == parseInt(val.id)) {
-        // console.log("at least", true, "as matches", val);
+        // console.log("at least", true, "as matches", idx);
+        /* if (x == totalRange[0]) console.log("if clause", allHTMLElements); */
         val.style.display = "none";
         val.parentElement.style.visibility = "hidden";
+        // allHTMLElements.splice(x, 1)
+      } 
+      else {
+       //  val.parentElement.setAttribute()
       }
     })
   })
@@ -71,7 +114,7 @@ onMount(()=>{
   {#each Object.values(row) as col}
   <span id={colID += 1} class="element-col">
     <!-- {console.log(col)} -->
-    {col.replace('element', `${prevRow += 1}`)}
+    <p name="no">{col.replace('element', `${prevRow += 1}`)}</p>
     <!-- inner Svelte component with its styling -->
     <Element />
   </span>
